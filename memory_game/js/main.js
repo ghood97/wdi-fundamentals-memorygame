228c70ad479c902a3937b976b057bb116b421f15
Griffin Hood <griffinhood@verizon.net>
2019-08-22 21:07:30 -0400

    Created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,19 @@
-var cardOne="queen";
-var cardTwo="queen";
-var cardThree="king";
-var cardFour="king";
-console.log("User flipped "+cardOne);
-console.log("User flipped "+cardThree);
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+let cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+if(cardsInPlay.length === 2)
+{
+  if(cardsInPlay[0] === cardsInPlay[1])
+  {
+    alert("You found a match!");
+  }
+  else
+  {
+    alert("Sorry, try again.");
+  }
+}
