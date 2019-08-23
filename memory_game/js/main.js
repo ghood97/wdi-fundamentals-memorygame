3786699a00151a71189ae2821da02ad6d71a7e6d
Griffin Hood <griffinhood@verizon.net>
2019-08-23 15:32:51 -0400

    more game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,7 @@
-const cards = ["queen", "queen", "king", "king"];
+const cards = [{rank: "queen", suit: "hearts", cardImage: "images/queen-of-hearts.png"},
+               {rank: "queen", suit: "diamonds", cardImage: "images/queen-of-diamonds.png"},
+               {rank: "king", suit: "hearts", cardImage: "images/king-of-hearts.png"},
+               {rank: "king", suit: "diamonds", cardImage: "images/kind-of-diamonds.png"}];
 const cardsInPlay = [];
 function checkMatch()
 {
@@ -13,12 +16,14 @@ function checkMatch()
 }
 function flipCard(cardId)
 {
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
   if(cardsInPlay.length === 2)
   {
     checkMatch();
   }
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
 };
 flipCard(0);
 flipCard(2);
