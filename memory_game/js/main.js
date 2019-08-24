4155f4fc9eae730847d28efed54dc521db8cf79b
Griffin Hood <griffinhood@verizon.net>
2019-08-23 23:44:07 -0400

    created game logic, added score and reset button

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,29 +1,67 @@
 const cards = [{rank: "queen", suit: "hearts", cardImage: "images/queen-of-hearts.png"},
                {rank: "queen", suit: "diamonds", cardImage: "images/queen-of-diamonds.png"},
                {rank: "king", suit: "hearts", cardImage: "images/king-of-hearts.png"},
-               {rank: "king", suit: "diamonds", cardImage: "images/kind-of-diamonds.png"}];
-const cardsInPlay = [];
+               {rank: "king", suit: "diamonds", cardImage: "images/king-of-diamonds.png"}];
+let cardsInPlay = [];
+let score = 0;
+
+function disable()
+{
+  let cardElements = document.querySelectorAll('img');
+  for(let i=0; i<cardElements.length; i++)
+  {
+    cardElements[i].removeEventListener('click', flipCard);
+  }
+}
+
+function reset()
+{
+  cardsInPlay = [];
+  let cardsToReset = document.querySelectorAll('img');
+  for(let i=0; i<cardsToReset.length; i++)
+  {
+    cardsToReset[i].setAttribute('src', 'images/back.png');
+    cardsToReset[i].addEventListener('click', flipCard);
+  }
+  document.getElementById('score').textContent = "Score: " + score;
+
+}
+
+function createBoard()
+{
+  for(let i=0; i<cards.length; i++)
+  {
+    let cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+    document.getElementById('reset').addEventListener('click', reset);
+  }
+}
+
 function checkMatch()
 {
   if(cardsInPlay[0] === cardsInPlay[1])
   {
     alert("You found a match!");
+    score++;
   }
   else
   {
     alert("Sorry, try again.");
   }
 }
-function flipCard(cardId)
+
+function flipCard()
 {
-  console.log("User flipped " + cards[cardId].rank);
+  let cardId = this.getAttribute('data-id');
   cardsInPlay.push(cards[cardId].rank);
+  this.setAttribute('src', cards[cardId].cardImage);
   if(cardsInPlay.length === 2)
   {
+    disable();
     checkMatch();
   }
-  console.log(cards[cardId].cardImage);
-  console.log(cards[cardId].suit);
 };
-flipCard(0);
-flipCard(2);
+createBoard();
